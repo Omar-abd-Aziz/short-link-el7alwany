81ceba728d536f2a94a459e6ec44b7986bfa3773
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2022-12-01 22:19:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
 
-    let ArrayOfLinks=localStorage.getItem("ArrayOfLinks")||[];
-
-    window.onload=()=>{
-        let ArrayOfLinks=localStorage.getItem("ArrayOfLinks");
-        ArrayOfLinks=eval(ArrayOfLinks);
-        const url = window.location.href;
-        const searchParams = new URL(url).searchParams; 
-        const urlSearchParams = new URLSearchParams(searchParams);
-        let id = urlSearchParams.get('id'); // Returns value of param1
-        if(id!==null){
-            ArrayOfLinks.forEach(e=>{
-                if(id==e.id){
-                    window.location.href=e.url
-                }
-            })
+        let ArrayOfLinks = localStorage.getItem("ArrayOfLinks");
+        if(ArrayOfLinks==null){
+            ArrayOfLinks=[];
+        } else{
+            ArrayOfLinks=eval(ArrayOfLinks);
+            const url = window.location.href;
+            const searchParams = new URL(url).searchParams; 
+            const urlSearchParams = new URLSearchParams(searchParams);
+            let id = urlSearchParams.get('id'); // Returns value of param1
+            if(id!==null){
+                let boool1 = ArrayOfLinks.find(e=>e.id==`${id}`);
+                window.location.href=boool1.url
+            }
         }
 
-    }
+
 
     document.querySelector('#btn-short').addEventListener('click', ()=>{
-        createShort();
-    });
 
-    function createShort() {
         let newURL=document.querySelector('#newURL').value;
+
         if(newURL!==''){
-            let ArrayOfLinks=localStorage.getItem("ArrayOfLinks");
-            ArrayOfLinks=eval(ArrayOfLinks);
-            document.querySelector('#newURL').value='';
-            let id = Math.floor(Math.random() * 10000)
-            ArrayOfLinks.push({
-                "id": id,
-                "url": newURL
-            });
-
-            let x = ArrayOfLinks;
             
-            localStorage.setItem("ArrayOfLinks",JSON.stringify(ArrayOfLinks));
+            let boool = ArrayOfLinks.find(e=>e.url===`${newURL}`);
+                
+            if(boool!==undefined){
+                document.querySelector('#newURL').value='';
+                document.querySelector('#out-but').href=window.location.origin + window.location.pathname+`?id=${boool.id}`;
+                document.querySelector('#out-but').innerHTML=window.location.origin + window.location.pathname+`?id=${boool.id}`;
+            }else if(boool===undefined){
+                document.querySelector('#newURL').value='';
+                let id = Math.floor(Math.random() * 10000);
+                
+                ArrayOfLinks.push({
+                    "id": id,
+                    "url": newURL
+                });
 
-            document.querySelector('#out-but').href=window.location.origin + window.location.pathname+`?id=${id}`;
-            document.querySelector('#out-but').innerHTML=window.location.origin + window.location.pathname+`?id=${id}`;
+                localStorage.setItem("ArrayOfLinks",JSON.stringify(ArrayOfLinks));
+                document.querySelector('#out-but').href=window.location.origin + window.location.pathname+`?id=${id}`;
+                document.querySelector('#out-but').innerHTML=window.location.origin + window.location.pathname+`?id=${id}`;
+        
+            }
         }
-    };
+    });
 
